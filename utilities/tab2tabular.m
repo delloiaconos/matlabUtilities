52ff445ec14cc63d1bd4d94321edeb238657dd51
Salvatore Dello Iacono <delloiaconos@alice.it>
2024-10-15 22:45:57 +0200

    Added header to function

--- utilities/tab2tabular.m
+++ utilities/tab2tabular.m
@@ -1,3 +1,22 @@
+%% 
+ % mplotUtility: MATLAB Common Utilities 
+ % Copyright (C) 2024 Salvatore Dello Iacono
+ %
+ %
+ % This is free software; you can redistribute it and/or modify
+ % it under the terms of the GNU General Public License as published by
+ % the Free Software Foundation; either version 3 of the License, or
+ % (at your option) any later version.
+ %
+ % This is distributed in the hope that it will be useful,
+ % but WITHOUT ANY WARRANTY; without even the implied warranty of
+ % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ % GNU General Public License for more details.
+ %
+ %  You should have received a copy of the GNU General Public License
+ %  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ %
+
 function tab2tabular( tbl, fName, varargin )
 %TAB2TABULAR Convert a table to a LaTeX Tabular Structure
 %   tbl: table to be converted
